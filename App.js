9e3606b49936ed50d052f08fffef144bfddde9e9
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-06-18 10:58:39 +0600

    add navigator

--- App.js
+++ App.js
@@ -1,12 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/screens/home';
+import Signin from './src/screens/signin';
+import Signup from './src/screens/signup';
+import Create from './src/screens/create';
+import Edit from './src/screens/edit';
+
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const user = false;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {
+          user ? (
+            <>
+              <Stack.Screen name="Home" component={Home} />
+              <Stack.Screen name="Create" component={Create} />
+              <Stack.Screen name="Edit" component={Edit} />
+            </>
+          ) : (
+            <>
+              <Stack.Screen name="Signin" component={Signin} />
+              <Stack.Screen name="Signup" component={Signup} />
+            </>
+          )
+        }
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
