894b7b799b117bf2dc262112d4d105f238702063
Md. Mazharul Islam <mazharulislam611@gmail.com>
2022-06-19 21:14:39 +0600

    radio button

--- src/screens/signup.jsx
+++ src/screens/signup.jsx
@@ -4,6 +4,9 @@ import Button from '../components/Button'
 import Input from './../components/Input';
 
 export default function Signup() {
+
+    const selected = true;
+
   return (
     <SafeAreaView>
         <Image 
@@ -18,11 +21,24 @@ export default function Signup() {
             <Input placeholder='Age' />
         </View>
 
-        <Pressable>
-            <View>
-                <Text>Male</Text> 
-                <Text>Female</Text> 
+        <Pressable style={styles.radioContainer}>
+            <View style={styles.outerCircle}>
+                <View style={styles.innerCircle}/>
             </View>
+            <Text style={styles.radioText}>Male</Text> 
+        </Pressable>
+
+        <Pressable style={styles.radioContainer}>
+            <View 
+                style={[styles.outerCircle, selected && styles.selectedOuterCircle]}
+            >
+                <View 
+                    style={[styles.innerCircle, selected 
+                    && 
+                    styles.selectedInnerCircle]}
+                />
+            </View>
+            <Text style={styles.radioText}>Female</Text> 
         </Pressable>
 
 
@@ -57,4 +73,37 @@ const styles = StyleSheet.create({
         paddingHorizontal: 26,
         paddingVertical: 26
     },
+
+    radioContainer: {
+        flexDirection: 'row',
+        alignItems: 'center', 
+        marginBottom: 10,
+        marginLeft: 30,
+    },
+    outerCircle: {
+        height: 30,
+        width: 30,
+        borderRadius: 15,
+        borderWidth: 1,
+        borderColor: '#cfcfcf',
+        justifyContent: 'center', 
+        alignItems: 'center'
+
+    },
+    innerCircle: {
+        height: 15,
+        width: 15,
+        borderRadius: 7.5,
+        borderWidth: 1,
+        borderColor: '#cfcfcf',
+    },
+    radioText: {
+        marginLeft: 10,
+    },
+    selectedOuterCircle: {
+        borderColor: 'orange',
+    },
+    selectedInnerCircle: {
+        backgroundColor: 'orange'
+    }
 })
